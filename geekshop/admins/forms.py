0d85b3e54b7f5a5d4b6e07aa4314debd6c829666
Daniil-Kapik <dann_kap@mail.ru>
2021-12-08 17:51:49 +0300

    creation forms, urls, views

--- geekshop/admins/forms.py
+++ geekshop/admins/forms.py
@@ -1,7 +1,9 @@
 from django import forms
+from django.contrib.auth.forms import UserChangeForm
 
 from authapp.forms import UserRegisterForm, UserProfileForm
 from authapp.models import User
+from mainapp.models import ProductCategory, Product
 
 
 class UserAdminRegisterForm(UserRegisterForm):
@@ -33,3 +35,61 @@ class UserAdminProfileForm(UserProfileForm):
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control py-4'
         self.fields['image'].widget.attrs['class'] = 'custom_file_input'
+
+
+class AdminCategoryCreateForm(forms.ModelForm):
+    name = forms.CharField(widget=forms.TextInput())
+    description = forms.CharField(widget=forms.TextInput())
+
+    class Meta:
+        model = ProductCategory
+        fields = ('name', 'description')
+
+    def __init__(self, *args, **kwargs):
+        super(AdminCategoryCreateForm, self).__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
+
+
+
+class AdminCategoryUpdateForm(UserChangeForm):
+
+    name = forms.CharField(widget=forms.TextInput())
+    description = forms.CharField(widget=forms.TextInput())
+
+    class Meta:
+        model = ProductCategory
+        fields = ('name', 'description')
+
+    def __init__(self, *args, **kwargs):
+        super(AdminCategoryUpdateForm, self).__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
+
+
+class AdminProductCreateForm(forms.ModelForm):
+    name = forms.CharField(widget=forms.TextInput())
+    image = forms.ImageField(widget=forms.FileInput(), required=False)
+    price = forms.IntegerField(widget=forms.NumberInput())
+    description = forms.CharField(widget=forms.TextInput())
+    quantity = forms.IntegerField(widget=forms.NumberInput())
+
+    class Meta:
+        model = Product
+        fields = ('name', 'image', 'price', 'description', 'quantity', 'category')
+
+    def __init__(self, *args, **kwargs):
+        super(AdminProductCreateForm, self).__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
+        self.fields['image'].widget.attrs['class'] = 'custom_file_input'
+        self.fields['category'].widget.attrs['class'] = 'related-widget-wrapper'
+
+
+
+
+
+
