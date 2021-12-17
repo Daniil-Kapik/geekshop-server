75fb18a45f816c20fc1e4db039b41894b80bb70e
Daniil-Kapik <dann_kap@mail.ru>
2021-12-17 13:15:37 +0300

    part_2_lesson_1

--- geekshop/authapp/urls.py
+++ geekshop/authapp/urls.py
@@ -1,13 +1,14 @@
 
 from django.urls import path
-from authapp.views import register, login, logout, profile
+from authapp.views import RegisterListView, LoginListView, Logout, ProfileFormView
 
 app_name = 'authapp'
 urlpatterns = [
-    path('register/', register, name='register'),
-    path('login/', login, name='login'),
-    path('logout/', logout, name='logout'),
-    path('profile/', profile, name='profile'),
+    path('register/', RegisterListView.as_view(), name='register'),
+    path('login/', LoginListView.as_view(), name='login'),
+    path('logout/', Logout.as_view(), name='logout'),
+    path('profile/', ProfileFormView.as_view(), name='profile'),
+    path('verify<str:email>/<str:activate_key>/', RegisterListView.verify, name='verify'),
 ]
 
 
